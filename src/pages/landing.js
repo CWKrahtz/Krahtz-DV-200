ee8e95409f85776d7f0d93e21488fcfd009099a6
Christian Krahtz <221198@virtualwindow.co.za>
2023-03-15 16:04:18 +0200

    Update landing.js
    Added cards and change data information displayed

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,14 +1,14 @@
 import React from "react";
 // import { useState, useEffect } from "react";
 // import axios from "axios";
-// import { Card } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import Carousel from "react-bootstrap/Carousel";
 import Marvel from '../Marvel.png'
 import VS from '../VS.png'
 import DC from '../DC.png'
 
 
-function Landing(){
+function Landing() {
 
     // const [apiData, setApiData] = useState([]);
     // const [missionName, setMissionName] = useState("Loading");
@@ -37,79 +37,92 @@ function Landing(){
     //     })
     // }, [])
 
-    return(
+    return (
         <div className="App">
-{/* <Card style={{ width: '20%', marginLeft: '10%', marginTop: '10%', float: 'left'}}>
-    <Card.Img variant="top" src={Marvel}  />
-        <Card.Body>
-            <Card.Title>About The API</Card.Title>
-            <Card.Text>
-                The API is a data source containing information about heroes and villains from both universes - Marvel and DC. 
-            </Card.Text>
-        </Card.Body>
-        </Card>
+            <Carousel fade style={{ marginTop: '5%' }}>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={Marvel}
+                        alt="First slide"
+                        style={{ marginLeft: 'auto', marginRight: 'auto' }}
+                    />
+                    <Carousel.Caption>
+                        <h3>About The API</h3>
+                        <p>The API is a data source containing information about heroes and villains from both universes - Marvel and DC.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={VS}
+                        alt="Second slide"
+                        style={{ marginLeft: 'auto', marginRight: 'auto' }}
+                    />
 
-        <Card style={{ width: '20%', marginLeft: '10%', marginTop: '10%', float: 'left'}}>
-        <Card.Img variant="top" src={VS}  />
+                    <Carousel.Caption>
+                        <h3>Reason for API</h3>
+                        <p>The data that is given can be used to see more information about the heroes and villains or it can be used to compare two characters to see who has a more favorable outcome according to the stats that are provided.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={DC}
+                        alt="Third slide"
+                        style={{ marginLeft: 'auto', marginRight: 'auto' }}
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Outline of API</h3>
+                        <p>
+                            The data used can give you more information about a specific character.You will be able to see their <b>power stats</b>, <b>biography</b>, <b>appearance</b>, <b>work</b>, <b>connections</b> and an image.
+                        </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+
+            <Card style={{ width: '25%', marginLeft: '7.5%', marginTop: '5%', float: 'left' }}>
+                <Card.Img variant="top" src={Marvel} style={{ height: '100px' }} />
                 <Card.Body>
-                    <Card.Title>Reason for API</Card.Title>
+                    <Card.Title>About Creator</Card.Title>
                     <Card.Text>
-                    The data that is given can be used to see more information about the heroes and villains or it can be used to compare two characters to see who has a more favorable outcome according to the stats that are provided.
+                        Name: Christian Krahtz<br/>
+                        Email: 221198@virtualwindow.co.za<br/>
+                        Student Number: 221198
                     </Card.Text>
                 </Card.Body>
-            </Card> 
-            <Card style={{ width: '20%', marginLeft: '10%', marginTop: '10%', float: 'left'}}>
-            <Card.Img variant="top" src={DC}  />
+            </Card>
+
+            <Card style={{ width: '25%', marginLeft: '5%', marginTop: '5%', float: 'left' }}>
+                <Card.Img variant="top" src={DC} style={{ height: '100px' }} />
                 <Card.Body>
-                    <Card.Title>Outline Of Data</Card.Title>
+                    <Card.Title>Information Breakdown</Card.Title>
                     <Card.Text>
-                    The data used can give you more information about a specific character.You will be able to see their power stats (Intelligence, Strength, Speed, Durability, Power and Combat) , biography (Full Name, Alter Egos, Aliases, Place of Birth, First Appearance, Publisher, Alignment), appearance (Gender, Race, Height, Weight, Eye Color, Hair Color), work (Occupation, Base of operation), connections (Group Affiliation, Relatives) and an image.
+                        <b>Power Stats:</b>
+                        (Intelligence, Strength, Speed, Durability, Power and Combat)<br />
+                        <b>Biography:</b>
+                        (Full Name, Alter Egos, Aliases, Place of Birth, First Appearance, Publisher, Alignment)<br />
+                        <b>Appearence:</b>
+                        (Gender, Race, Height, Weight, Eye Color, Hair Color)<br />
+                        <b>Work:</b>
+                        (Occupation, Base of operation)<br />
+                        <b>Connections:</b>
+                        (Group Affiliation, Relatives)
                     </Card.Text>
-    </Card.Body>
-</Card>  */}
-           
-            <Carousel fade style={{marginTop:'10%'}}>
-            <Carousel.Item>
-                <img
-                className="d-block w-100"
-                src= {Marvel}
-                alt="First slide"
-                style={{marginLeft: 'auto', marginRight: 'auto'}}
-                />
-                <Carousel.Caption>
-                <h3>About The API</h3>
-                <p>The API is a data source containing information about heroes and villains from both universes - Marvel and DC.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                className="d-block w-100"
-                src={VS}
-                alt="Second slide"
-                style={{marginLeft: 'auto', marginRight: 'auto'}}
-                />
+                </Card.Body>
+            </Card>
 
-                <Carousel.Caption>
-                <h3>Reason for API</h3>
-                <p>The data that is given can be used to see more information about the heroes and villains or it can be used to compare two characters to see who has a more favorable outcome according to the stats that are provided.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                className="d-block w-100"
-                src={DC}
-                alt="Third slide"
-                style={{marginLeft: 'auto', marginRight: 'auto'}}
-                />
+            <Card style={{ width: '25%', marginLeft: '5%', marginTop: '5%', float: 'left' }}>
+                <Card.Img variant="top" src={VS} style={{ height: '100px' }} />
+                <Card.Body>
+                    <Card.Title>More on the API</Card.Title>
+                    <Card.Text>
+                        There are about 700 plus characters you can look at nd compare with others. you can compare Yoda and The Hulk with eachother and see who has the more impressive character data.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
 
-                <Carousel.Caption>
-                <h3>Outline of API</h3>
-                <p>
-                The data used can give you more information about a specific character.You will be able to see their power stats (Intelligence, Strength, Speed, Durability, Power and Combat) , biography (Full Name, Alter Egos, Aliases, Place of Birth, First Appearance, Publisher, Alignment), appearance (Gender, Race, Height, Weight, Eye Color, Hair Color), work (Occupation, Base of operation), connections (Group Affiliation, Relatives) and an image.
-                </p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            </Carousel>
         </div>
     )
 }
